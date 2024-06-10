be16e17d8496dcf6571c87512fa371b8ba55b648
Oskar Liew <oskar@liew.se>
2024-06-10 18:20:49 +0200

    NVIDIA specialization

--- machines/hopfield/default.nix
+++ machines/hopfield/default.nix
@@ -24,37 +24,37 @@
     "nvme.noacpi=1"
   ];
 
-  # hardware.opengl = {
-  #     enable = true;
-  #     driSupport = true;
-  #     driSupport32Bit = true;
-  # };
+  hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+  };
 
   hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-      # modesetting.enable = true;
+      modesetting.enable = true;
 
       prime = {
-          # offload = {
-          #     enable = true;
-          #     enableOffloadCmd = true;
-          # };
+          offload = {
+              enable = true;
+              enableOffloadCmd = true;
+          };
           nvidiaBusId = "PCI:1:0:0";
           intelBusId = "PCI:0:2:0";
       };
-
   };
-  #
-  # specialisation = {
-  #   nvidia-on.configuration = {
-  #       hardware.nvidia.prime= {
-  #           offload.enable = lib.mkForce false;
-  #           offload.enableOffloadCmd = lib.mkForce false;
-  #           sync.enable = lib.mkForce true;
-  #       };
-  #   };
-  # };
+
+  specialisation = {
+    nvidia-on.configuration = {
+        hardware.nvidia.prime= {
+            offload.enable = lib.mkForce false;
+            offload.enableOffloadCmd = lib.mkForce false;
+            sync.enable = lib.mkForce true;
+        };
+        services.xserver.videoDrivers = lib.mkForce ["nvidia"];
+    };
+  };
 
   console.keyMap = "sv-latin1";
 }
