f9175aaa7c8336b9a7caff51d9af25dd93355f9f
Oskar Liew <oskar@liew.se>
2025-10-02 20:39:50 +0200

    No Mason on nix

--- home/config/nvim/lua/configs/plugins/mason.lua
+++ home/config/nvim/lua/configs/plugins/mason.lua
@@ -1,6 +1,13 @@
+local function using_nix()
+	return vim.env.IN_NIX_SHELL or vim.env.NIX_PROFILES or vim.env.NIX_PATH
+end
+
 return {
 	{
 		"mason-org/mason.nvim",
 		opts = {},
+		enabled = function()
+			return not using_nix()
+		end,
 	},
 }
