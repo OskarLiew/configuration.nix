601dfe0bc3ad6faf45f8ccc5242183067a7c5c7c
Oskar Liew <oskar@liew.se>
2025-10-02 21:35:40 +0200

    Blink config

--- home/config/nvim/lua/configs/plugins/blinkcmp.lua
+++ home/config/nvim/lua/configs/plugins/blinkcmp.lua
@@ -97,6 +97,14 @@ return {
 					},
 				},
 			},
+			completion = {
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+				menu = {
+					draw = {
+						columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "kind" } },
+					},
+				},
+			},
 		},
 		opts_extend = { "sources.default" },
 	},
