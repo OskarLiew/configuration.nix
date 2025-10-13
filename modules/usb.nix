65f8cfb1548152007be64e69d98125b3b43ec88e
Oskar Liew <oskar@liew.se>
2025-10-13 21:54:42 +0200

    usb mount on /media

--- modules/usb.nix
+++ modules/usb.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
   services.devmon.enable = true;
   services.gvfs.enable = true;
+
   services.udisks2.enable = true;
+  services.udisks2.mountOnMedia = true;
 
   boot.supportedFilesystems = [ "ntfs" ];
 
