8f3bd07778eb0b61a153d5b53528c62834f12445
Oskar Liew <oskar@liew.se>
2025-10-16 13:36:34 +0200

    Use ruff_format

--- home/config/nvim/lua/configs/plugins/conform.lua
+++ home/config/nvim/lua/configs/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
 		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {
-				python = { "ruff_organize_imports", "ruff_fix" },
+				python = { "ruff_format", "ruff_organize_imports" },
 				lua = { "stylua" },
 			},
 			format_on_save = {
