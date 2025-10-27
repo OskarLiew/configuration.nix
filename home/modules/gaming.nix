f3f253d2f832cf4e632be83d8daf58a527c9368e
Oskar Liew <oskar@liew.se>
2025-10-27 19:43:30 +0100

    Retroarch

--- home/modules/gaming.nix
+++ home/modules/gaming.nix
@@ -3,8 +3,17 @@
 {
   home.packages = with pkgs; [
     # Utils
-    path-of-building
     bolt-launcher # OSRS launcher for linux
     osu-lazer-bin
+    (retroarch.withCores (cores: with cores; [
+      gambatte # GB
+      mgba # GBA
+      fceumm # NES
+      snes9x # SNES
+      melonds # Nintendo DS
+      parallel-n64 # N64
+      dolphin # GC / Wii
+      citra # 3DS
+    ]))
   ];
 }
