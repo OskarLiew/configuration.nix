29d8f24d5e661fd3d636896fa5db4546ca4bc5e8
Oskar Liew <oskar@liew.se>
2025-11-03 20:16:00 +0100

    Awakened poe trade

---
+++ home/packages/awakened-poe-trade.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  fetchurl,
+  stdenv,
+  appimageTools,
+  makeWrapper,
+  electron,
+  xorg,
+  ver ? "3.27.101",
+}:
+
+stdenv.mkDerivation rec {
+  pname = "awakened-poe-trade";
+  version = "${ver}";
+
+  src = fetchurl {
+    url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v${version}/Awakened-PoE-Trade-${version}.AppImage";
+    hash = "sha256-aQaTuFdtavhCRqbm3StxpNeEndS7l91d4SWA61p5rrg=";
+  };
+
+  appimageContents = appimageTools.extractType2 {
+    inherit pname src version;
+  };
+
+  dontUnpack = true;
+  dontConfigure = true;
+  dontBuild = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin $out/share/awakened-poe-trade $out/share/applications
+
+    cp -a ${appimageContents}/{locales,resources} $out/share/awakened-poe-trade
+    cp -a ${appimageContents}/awakened-poe-trade.desktop $out/share/applications/
+    cp -a ${appimageContents}/usr/share/icons $out/share
+
+    substituteInPlace $out/share/applications/awakened-poe-trade.desktop \
+    --replace-fail 'Exec=AppRun' 'Exec=awakened-poe-trade'
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+        makeWrapper ${lib.getExe electron} $out/bin/awakened-poe-trade \
+    --add-flags $out/share/awakened-poe-trade/resources/app.asar \
+    --prefix LD_LIBRARY_PATH : "${
+      lib.makeLibraryPath [
+        xorg.libXtst
+        xorg.libXt
+      ]
+    }"
+  '';
+
+  meta = {
+    description = "Path of Exile trading app for price checking";
+    homepage = "https://github.com/SnosMe/awakened-poe-trade";
+    changelog = "https://github.com/SnosMe/awakened-poe-trade/releases/tag/v${version}";
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.nezia ];
+    platforms = lib.platforms.linux;
+    mainProgram = "awakened-poe-trade";
+  };
+}
