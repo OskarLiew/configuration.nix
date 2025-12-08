2252aeaa74602de1a4730af52fda0871931bfea8
Oskar Liew <oskar@liew.se>
2025-12-08 20:54:18 +0100

    25.11

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
@@ -19,7 +19,7 @@
       url = "github:kosorin/awesome-code-doc";
       flake = false;
     };
-    stylix.url = "github:nix-community/stylix/release-25.05";
+    stylix.url = "github:nix-community/stylix/release-25.11";
   };
 
   outputs =
