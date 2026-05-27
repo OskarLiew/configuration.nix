f82e0897deec35f070a2c6febaa03cfbb3d5a01a
Oskar Liew <oskar@liew.se>
2026-05-27 20:33:02 +0200

    Slim down plasma

--- nixos/modules/extra/plasma.nix
+++ nixos/modules/extra/plasma.nix
@@ -11,15 +11,8 @@
     kdePackages.kcalc # Calculator
     kdePackages.kcharselect # Character map
     kdePackages.kclock # Clock app
-    kdePackages.kcolorchooser # Color picker
-    kdePackages.kolourpaint # Simple paint program
-    kdePackages.ksystemlog # System log viewer
-    kdePackages.sddm-kcm # SDDM configuration module
-    kdiff3 # File/directory comparison tool
 
     # Hardware/System Utilities (Optional)
-    kdePackages.isoimagewriter # Write hybrid ISOs to USB
-    kdePackages.partitionmanager # Disk and partition management
     hardinfo2 # System benchmarks and hardware info
     wayland-utils # Wayland diagnostic tools
     wl-clipboard # Wayland copy/paste support
--- nixos/users/oskar.nix
+++ nixos/users/oskar.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   user = "oskar";
-  name = "Caroline Liew";
+  name = "Oskar Liew";
 in
 {
   imports = [ ../modules/extra/awesome.nix ];
